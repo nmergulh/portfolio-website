facc1919929e92d1f549a6e4be1d7b165d58bf12
Neil M <nmergulh.live.com>
2021-12-31 22:38:05 +0000

    added hamburger menu with anchors to different page sections

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 import "./app.scss";
 import { useState } from "react";
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,19 @@
+import "./menu.scss";
+
+export default function menu({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"menu " + (menuOpen && "active")}>
+      <ul>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="#intro">Home</a>
+        </li>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
